3646e01fe90e210df566fc3601a7f6de2315862f
almu8322 <luis.martinez@sjsu.edu>
2022-06-23 02:28:40 -0700

    first changes 1

--- src/routes/SinglePost.js
+++ src/routes/SinglePost.js
@@ -1,26 +1,29 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-
-const SinglePost = ({token}) => {
-  const {postId} = useParams();
+const SinglePost = ({ token }) => {
+  const { postId } = useParams();
   console.log(postId);
   console.log(token);
-  fetch(`https://strangers-things.herokuapp.com/api/2202-ftb-pt-web-pt/posts/${postId}/messages`, {
-  method: "POST",
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-  },
-  body: JSON.stringify({
-    message: {
-      content: "Do you still have this?  Would you take $10 less?"
+  fetch(
+    `https://strangers-things.herokuapp.com/api/2202-ftb-pt-web-pt/posts/${postId}/messages`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        message: {
+          content: "Do you still have this?  Would you take $10 less?",
+        },
+      }),
     }
-  })
-}).then(response => response.json())
-  .then(result => {
-    console.log(result);
-  })
-  .catch(console.error);
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
   return (
     <main>
       <h1>Single Post</h1>
@@ -29,3 +32,5 @@ const SinglePost = ({token}) => {
 };
 
 export default SinglePost;
+asdfsf;
+//random comment.
